2f62ff9327ba61d52aba0b0d2e5f8b67e1e3302a
devdeM1 <makd1232255@yandex.ru>
2025-08-14 11:51:25 +0300

    Fix namming. Edit chorome to chromium on driver

--- tests/park_tests.py
+++ tests/park_tests.py
@@ -1,7 +1,7 @@
 from conftest import driver
 from pages.residents_page import ResidentsPage
 
-def test_basic_auth(driver):
-    auth_page = ResidentsPage(driver)
-    auth_page.open()
-    assert auth_page.page_is_successfully_open(auth_page.TITLE), 'The page is not opened correctly'
+def test_resident_page(driver):
+    resident_page = ResidentsPage(driver)
+    resident_page.open()
+    assert resident_page.page_is_successfully_open(resident_page.TITLE), 'The page is not opened correctly'
