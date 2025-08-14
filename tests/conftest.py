544df098ab9f913e9705c492f5bebae027064274
devdeM1 <makd1232255@yandex.ru>
2025-08-14 11:34:21 +0300

    Edit driver

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,10 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="function")
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
